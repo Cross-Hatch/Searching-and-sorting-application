4f16b21394097e3f32478704fe98954ca288530d
GideonAg <gideonagbosu72@gmail.com>
2022-08-09 22:33:59 +0000

    Modified Main
    fixed some bugs

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,18 @@ public class Main {
 
         int [] aa = {3, 5, 1 , 8, 7, 2, 91, 2};
 
-        System.out.println("Searching or sorting: ");
+        System.out.print("Enter 1 for searching or 2 for sorting: ");
         String selectedInput = userInput.nextLine();
 
-        if(selectedInput.equalsIgnoreCase("Sorting")){
+        if(selectedInput.equalsIgnoreCase("one") || selectedInput.equals("1")){
             System.out.println("Enter sorting algorithm preferred");
-            String algoSelected = userInput.nextLine();
-            new SearchingAndSortingAlgorithm(algoSelected, aa);
-        } else if (selectedInput.equalsIgnoreCase("Searching")) {
+            String algoSelected = userInput.nextLine().trim();
+            if (algoSelected.equalsIgnoreCase("bubbleSort") || algoSelected.equalsIgnoreCase("selectionSort")){
+                new SearchingAndSortingAlgorithm(algoSelected, aa);
+            }else {
+                System.out.println("Sorry, sorting algorithm not found!");
+            }
+        } else if (selectedInput.equalsIgnoreCase("two") || selectedInput.equals("2")) {
             System.out.println("Enter searching algorithm preferred");
             String algoSelected = userInput.nextLine();
             System.out.println("Enter number to search for");
@@ -26,5 +30,4 @@ public class Main {
         }
 
     }
-
 }
