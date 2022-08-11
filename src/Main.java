0d3da5eeb8a05c8a7e7a5bde52179456511406cb
GideonAg <gideonagbosu72@gmail.com>
2022-08-11 08:00:42 +0000

    Modified Main
    Handled exceptions, implemented a while loop to run program as long as user wants to.

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,46 @@ public class Main {
     public static void main(String [] args){
 
         Scanner userInput = new Scanner(System.in);
+        boolean status = true;
 
-        System.out.println("Enter array");
-        String input = userInput.nextLine();
+        while (status){
+            try{
+                System.out.println();
+                System.out.println("Enter array of numbers separated by single space only or Q to quit");
+                String input = userInput.nextLine();
 
-        int [] array = StringArrayToIntegerArray.method(input);
+                if (!input.equalsIgnoreCase("Q")) {
+                    int[] array = StringArrayToIntegerArray.method(input);
 
-        System.out.print("Enter 1 for sorting or 2 for searching: ");
-        String selectedInput = userInput.nextLine();
+                    System.out.print("Enter 1 for sorting or 2 for searching or Q to quit: ");
+                    String selectedInput = userInput.nextLine();
 
-        if(selectedInput.equalsIgnoreCase("one") || selectedInput.equals("1")){
-            System.out.println("Enter sorting algorithm preferred");
-            String algoSelected = userInput.nextLine().trim();
-            if (algoSelected.equalsIgnoreCase("bubbleSort") || algoSelected.equalsIgnoreCase("selectionSort")){
-                new SearchingAndSortingAlgorithm(algoSelected, array);
-            }else {
-                System.out.println("Sorry, sorting algorithm not found!");
+                    if (selectedInput.equalsIgnoreCase("one") || selectedInput.equals("1")) {
+                        System.out.println("Enter sorting algorithm preferred");
+                        String algoSelected = userInput.nextLine().trim();
+                        if (algoSelected.equalsIgnoreCase("bubbleSort") || algoSelected.equalsIgnoreCase("selectionSort")) {
+                            new SearchingAndSortingAlgorithm(algoSelected, array);
+                        } else {
+                            System.out.println("Sorry, sorting algorithm not found!");
+                        }
+                    } else if (selectedInput.equalsIgnoreCase("two") || selectedInput.equals("2")) {
+                        System.out.println("Enter searching algorithm preferred");
+                        String algoSelected = userInput.nextLine();
+                        System.out.println("Enter number to search for");
+                        int number = userInput.nextInt();
+                        new SearchingAndSortingAlgorithm(algoSelected, array, number);
+                    } else if (selectedInput.equalsIgnoreCase("q")) {
+                        status = false;
+                    } else {
+                        System.out.println("Oops! Invalid input");
+                    }
+                }else {
+                    status = false;
+                    System.out.println("Have a nice day!");
+                }
+            }catch (NumberFormatException exception){
+                System.out.println("Invalid input");
             }
-        } else if (selectedInput.equalsIgnoreCase("two") || selectedInput.equals("2")) {
-            System.out.println("Enter searching algorithm preferred");
-            String algoSelected = userInput.nextLine();
-            System.out.println("Enter number to search for");
-            int number = userInput.nextInt();
-            new SearchingAndSortingAlgorithm(algoSelected, array, number);
-        }else{
-            System.out.println("Oops! Invalid input");
         }
 
     }
