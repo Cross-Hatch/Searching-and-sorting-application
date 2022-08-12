38bb2ae439b4aa3df73f5df15a517e05ce74fe11
GideonAg <gideonagbosu72@gmail.com>
2022-08-12 23:39:01 +0000

    Added Binary search

--- src/Algorithms/SearchingAlgorithms.java
+++ src/Algorithms/SearchingAlgorithms.java
@@ -12,4 +12,21 @@ public class SearchingAlgorithms {
         return -1;
     }
 
+    // Works for sorted arrays
+    public static int binarysearch(int [] array, int value){
+
+        int low = 0;
+        int high = array.length -1;
+
+        while (low <= high){
+            int middle = low + (high - low) / 2;
+            int target = array[middle];
+
+            if(target < value) { low = middle + 1;}
+            else if (target > value){ high = middle - 1;}
+            else{ return middle;}
+        }
+        return -1;
+    }
+
 }
