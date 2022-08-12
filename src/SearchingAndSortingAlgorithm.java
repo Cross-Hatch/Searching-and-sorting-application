dee2de14b4a4ff66b165dcb9bbb597d008c2a116
GideonAg <gideonagbosu72@gmail.com>
2022-08-12 23:40:55 +0000

    Modified SearchingAndSortingAlgorithm
    Added binary search

--- src/SearchingAndSortingAlgorithm.java
+++ src/SearchingAndSortingAlgorithm.java
@@ -1,3 +1,4 @@
+import static Algorithms.SearchingAlgorithms.binarysearch;
 import static Algorithms.SearchingAlgorithms.linearSearch;
 import static Algorithms.SortingAlgorithms.bubbleSort;
 import static Algorithms.SortingAlgorithms.selectionSort;
@@ -8,13 +9,13 @@ public class SearchingAndSortingAlgorithm {
     //      SORTING OF ARRAYS
     SearchingAndSortingAlgorithm(String algorithm, int [] array){
         switch (algorithm){
-            case "bubblesort" -> {
+            case "1" -> {
                 bubbleSort(array);
                 for (int j : array) {
                     System.out.print(j + " ");
                 }
             }
-            case "selectionsort" -> {
+            case "2" -> {
                 selectionSort(array);
                 for (int j : array) {
                     System.out.print(j + " ");
@@ -26,12 +27,20 @@ public class SearchingAndSortingAlgorithm {
     //  SEARCHING THROUGH ARRAYS
     SearchingAndSortingAlgorithm(String algorithm, int  [] array, int searchNumber){
         switch (algorithm) {
-            case "linearsearch" -> {
+            case "1" -> {
                 int index = linearSearch(array, searchNumber);
                 if (index != -1) {
                     System.out.println("Element is at index: " + index);
                 } else {
                     System.out.println("Element not found");
+                }
+            }
+            case "2" -> {
+                int index = binarysearch(array, searchNumber);
+                if (index == -1) {
+                    System.out.println("Element not found");
+                } else {
+                    System.out.println("Element found at index " + index);
                 }
             }
         }
