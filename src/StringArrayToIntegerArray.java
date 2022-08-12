4d656dce6a8784b828940b4c9b7ea49c19943fc7
GideonAg <gideonagbosu72@gmail.com>
2022-08-12 11:50:20 +0000

    Added some comments

--- src/StringArrayToIntegerArray.java
+++ src/StringArrayToIntegerArray.java
@@ -1,5 +1,8 @@
+
+// Class for converting string array to integer array
 public class StringArrayToIntegerArray {
 
+    // method for converting string array to integer array
     public static int [] method(String array){
         String [] splitArray = array.split(" ");
         int [] integerArray = new int [splitArray.length];
