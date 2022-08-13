efc2559d25813f392d850cc88e0afb91814360d3
GideonAg <gideonagbosu72@gmail.com>
2022-08-13 02:13:16 +0000

    Modified SortingAlgorithms
    Added merge sort and quick sort

--- src/Algorithms/SortingAlgorithms.java
+++ src/Algorithms/SortingAlgorithms.java
@@ -29,4 +29,83 @@ public class SortingAlgorithms {
 
         }
     }
+
+    public static void mergeSort(int [] array){
+        int length = array.length;
+        if(length <= 1) return;
+        int middle = length / 2;
+        int [] leftArray = new int[middle];
+        int [] rightArray = new int[length - middle];
+
+        int i = 0;
+        int j = 0;
+
+        for (; i < length; i++){
+            if (i < middle){
+                leftArray[i] = array[i];
+            }else {
+                rightArray[j] = array[i];
+                j++;
+            }
+        }
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray, rightArray, array);
+    }
+    private static void merge(int [] leftArray, int [] rightArray, int [] array){
+
+        int leftSize = array.length / 2;
+        int rightSize = array.length - leftSize;
+        int i = 0, l = 0, r = 0;
+
+        while (l < leftSize && r < rightSize){
+            if (leftArray[l] < rightArray[r]){
+                array[i] = leftArray[l];
+                i++;
+                l++;
+            }else {
+                array[i] = rightArray[r];
+                i++;
+                r++;
+            }
+        }
+        while (l < leftSize){
+            array[i] = leftArray[l];
+            i++;
+            l++;
+        }
+        while (r < rightSize){
+            array[i] = rightArray[r];
+            i++;
+            r++;
+        }
+    }
+
+    public static void quickSort(int [] array, int start, int end){
+
+        if (end < start) return;
+        int pivot = partition(array, start, end);
+        quickSort(array, start, pivot - 1);
+        quickSort(array, pivot + 1, end);
+    }
+    public static int partition(int [] array, int start, int end){
+
+        int pivot = array[end];
+        int i = start - 1;
+
+        for (int j = start; j <= end - 1; j++){
+            if (array[j] < pivot){
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        i++;
+        int temp = array[i];
+        array[i] = array[end];
+        array[end] = temp;
+
+        return i;
+    }
 }
