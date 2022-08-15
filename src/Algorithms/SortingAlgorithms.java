7a5ce8dfd6e29cf45273c17344079441f6110f4b
GideonAg <gideonagbosu72@gmail.com>
2022-08-15 18:14:09 +0000

    Added radix sort to Sorting class

--- src/Algorithms/SortingAlgorithms.java
+++ src/Algorithms/SortingAlgorithms.java
@@ -1,6 +1,39 @@
 package Algorithms;
 
 public class SortingAlgorithms {
+    public static void radixSort(int [] array, int radix, int width){
+        for (int i = 0; i < width; i++){
+            radixSingleSort(array, i, radix);
+        }
+    }
+
+    private static void radixSingleSort(int [] array, int position, int radix){
+        int numItems = array.length;
+        int [] countArray = new int[radix];
+
+        for (int value: array){
+            countArray[getDigit(position, value, radix)]++;
+        }
+        // Adjust the count array
+        for (int j = 1; j < radix; j++){
+            countArray[j] += countArray[j - 1];
+        }
+
+        int [] temp = new int[numItems];
+        for (int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--){
+            temp[--countArray[getDigit(position, array[tempIndex], radix)]] =
+                    array[tempIndex];
+        }
+
+        for (int tempIndex = 0; tempIndex < numItems; tempIndex++){
+            array[tempIndex] = temp[tempIndex];
+        }
+    }
+
+    private static int getDigit(int position, int value, int radix){
+        return value / (int) Math.pow(10, position) % radix;
+    }
+
     public static void shellSort(int [] array){
         for (int gap = array.length / 2; gap > 0; gap /= 2){
 
@@ -118,7 +151,7 @@ public class SortingAlgorithms {
         quickSort(array, start, pivot - 1);
         quickSort(array, pivot + 1, end);
     }
-    public static int partition(int [] array, int start, int end){
+    private static int partition(int [] array, int start, int end){
 
         int pivot = array[end];
         int i = start - 1;
